75e3cfcf060798b4a8b89101fc4ff736df29d8fc
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-18 21:58:45 -0600

    Comment out Counter component import and usage in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
+// import { Counter } from './counter/components/Counter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* <Counter /> */}
   </StrictMode>
 );
