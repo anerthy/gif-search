3f2b804ad50da088a4f6f414bbe0ac4bd1e83442
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-18 22:56:09 -0600

    test: add unit tests for useCounter hook

---
+++ src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,54 @@
+import { describe, expect, test } from 'vitest';
+import { useCounter } from './useCounter';
+import { renderHook, act } from '@testing-library/react';
+
+describe('useCounter', () => {
+  test('should initialize with default value of 10', () => {
+    const { result } = renderHook(() => useCounter());
+    expect(result.current.count).toBe(10);
+  });
+
+  test('should initialize with value 5', () => {
+    const initialValue = 5;
+    const { result } = renderHook(() => useCounter(initialValue));
+    expect(result.current.count).toBe(initialValue);
+  });
+
+  test('should increment counter when handleAdd is called', () => {
+    const { result } = renderHook(() => useCounter(1));
+
+    act(() => {
+      result.current.handleAdd();
+    });
+
+    expect(result.current.count).toBe(2);
+  });
+
+  test('should decrement counter when handleSubtract is called', () => {
+    const { result } = renderHook(() => useCounter(2));
+
+    act(() => {
+      result.current.handleSubtract();
+    });
+    expect(result.current.count).toBe(1);
+  });
+
+  test('should not decrement counter when handleSubtract is called and count is 1', () => {
+    const { result } = renderHook(() => useCounter(1));
+
+    act(() => {
+      result.current.handleSubtract();
+    });
+
+    expect(result.current.count).toBe(0);
+  });
+
+  test('should reset counter to 0 when handleReset is called', () => {
+    const { result } = renderHook(() => useCounter(5));
+
+    act(() => {
+      result.current.handleReset();
+    });
+    expect(result.current.count).toBe(0);
+  });
+});
